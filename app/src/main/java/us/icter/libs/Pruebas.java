9923f3e7dd05d76cb695c3dc022dcc5a3268eefa
Jaro Marval <jampgold@gmail.com>
2016-05-12 22:16:56 -0400

    Preparando el primer candidato para ser liberado

--- app/src/main/java/us/icter/libs/Pruebas.java
+++ app/src/main/java/us/icter/libs/Pruebas.java
@@ -13,13 +13,37 @@ public class Pruebas extends ArrayList<Estacion> {
     public Pruebas() {
         Estacion estacion;
 
-        estacion = new Estacion("123", "Ebola", 1, 1, R.drawable.infografia_ebola);
+        estacion = new Estacion("pdc1_reto1", "Punto 1 - Reto 1", 1, 2, R.drawable.pdc1_reto1);
+        add(estacion);
+        estacion = new Estacion("pdc1_reto3", "Punto 1 - Reto 3", 1, 3, R.drawable.pdc1_reto2, 25);
+        add(estacion);
+        estacion = new Estacion("pdc1_reto4", "Punto 1 - Reto 4", 1, 2, R.drawable.pdc1_reto3);
+        add(estacion);
+        estacion = new Estacion("pdc1_reto2", "Punto 1 - Reto 2", 1, 2, R.drawable.pdc1_reto4);
+        add(estacion);
+        estacion = new Estacion("pdc1_reto5", "Punto 1 - Reto 5", 1, 2, R.drawable.pdc1_reto5);
         add(estacion);
 
-        estacion = new Estacion("133", "Redes Sociales", 1, 2, R.drawable.infografia_adictos_a_las_redes);
+        estacion = new Estacion("pdc2_reto1", "Punto 2 - Reto 1", 2, 3, R.drawable.pdc2_reto1, 25);
+        add(estacion);
+        estacion = new Estacion("pdc2_reto2", "Punto 2 - Reto 2", 2, 2, R.drawable.pdc2_reto2);
+        add(estacion);
+        estacion = new Estacion("pdc2_reto3", "Punto 2 - Reto 3", 2, 3, R.drawable.pdc2_reto3, 25);
+        add(estacion);
+        estacion = new Estacion("pdc2_reto4", "Punto 2 - Reto 4", 2, 2, R.drawable.pdc2_reto4);
+        add(estacion);
+        estacion = new Estacion("pdc2_reto5", "Punto 2 - Reto 5", 2, 2, R.drawable.pdc2_reto5);
         add(estacion);
 
-        estacion = new Estacion("134", "Prevención de Ebola", 1, 3, R.drawable.infografia_ebola, 5);
+        estacion = new Estacion("pdc3_reto1", "Punto 3 - Reto 1", 3, 3, R.drawable.pdc3_reto1, 25);
+        add(estacion);
+        estacion = new Estacion("pdc3_reto2", "Punto 3 - Reto 2", 3, 2, R.drawable.pdc3_reto2);
+        add(estacion);
+        estacion = new Estacion("pdc3_reto3", "Punto 3 - Reto 3", 3, 2, R.drawable.pdc3_reto3);
+        add(estacion);
+        estacion = new Estacion("pdc3_reto4", "Punto 3 - Reto 4", 3, 2, R.drawable.pdc3_reto4);
+        add(estacion);
+        estacion = new Estacion("pdc3_reto5", "Punto 3 - Reto 5", 3, 2, R.drawable.pdc3_reto5);
         add(estacion);
     }
 
